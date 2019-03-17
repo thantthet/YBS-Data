4f2aad6e5caa89682ccf6cc68075f3b60e3e7dd6
Thant Thet <t@ttkz.me>
2019-03-18 00:22:35 +0630

    make script importable

--- tools/make_stops_json.py
+++ tools/make_stops_json.py
@@ -7,17 +7,18 @@ import json
 import codecs
 
 def convert_stops():
-  def jsonlize(x):
-    x['id'] = int(x['id'])
-    x['lat'] = float(x['lat'])
-    x['lng'] = float(x['lng'])
-    return x
-  tsv_path = './../data/stops.tsv'
-  json_path = './../data/stops.json'
-  print 'Reading from %s' % tsv_path
-  rows = utils.readTsv(tsv_path)
-  with open(json_path, 'wb') as json_file:
-    print 'Writing to %s' % json_path
-    json_file.write(json.dumps(map(jsonlize, rows)))
+	def jsonlize(x):
+		x['id'] = int(x['id'])
+		x['lat'] = float(x['lat'])
+		x['lng'] = float(x['lng'])
+		return x
+	tsv_path = './../data/stops.tsv'
+	json_path = './../data/stops.json'
+	print 'Reading from %s' % tsv_path
+	rows = utils.readTsv(tsv_path)
+	with open(json_path, 'wb') as json_file:
+		print 'Writing to %s' % json_path
+		json_file.write(json.dumps(map(jsonlize, rows)))
 
-convert_stops()
+if __name__ == '__main__':
+	convert_stops()
